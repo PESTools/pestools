338d2679d698ca9079478f56cadf476ed2a1c3cd
echristi <evan.christianson2@gmail.com>
2014-12-13 13:54:22 -0600

    ParSen Plots working

--- pst_tools/parsen.py
+++ pst_tools/parsen.py
@@ -8,6 +8,7 @@ from pst import Pest
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import plots
 
 class ParSen(Pest):
     def __init__(self, basename, jco_df = None, drop_regul = False, 
@@ -54,7 +55,7 @@ class ParSen(Pest):
             
         Attributes
         ----------
-        par_sen_df : Pandas DataFrame 
+        df : Pandas DataFrame 
             DataFrame of parameter sensitivity.  Index entries of the DataFrame
             are the parameter names.  The DataFrame has two columns: 
             1) Parameter Group and 2) Sensitivity
@@ -81,23 +82,26 @@ class ParSen(Pest):
         ''' 
         if jco_df == None:              
             jco_df = self._load_jco()
+        self._read_par_data()
+        self._read_obs_data()
+        
         # Build obs dictionary
         # key is OBSNME values are (WEIGHT, OBGNME)
         obs_dict = {}
         for index, row in self.obsdata.iterrows():
-            obs_dict[index] = (row['WEIGHT'], row['OBGNME'])
+            obs_dict[index.lower()] = (row['WEIGHT'], row['OBGNME'].lower())
         # Also need to get prior info if present
         try:
-            priordata = self._read_prior()
-            for index, row in priordata:
-                obs_dict[index] = (row['WEIGHT'], row['OBGNME'])
+            self._read_prior()
+            for index, row in self.priordata.iterrows():
+                obs_dict[index.lower()] = (row['WEIGHT'], row['OBGNME'].lower())
         except:
             pass
         # Build pars_dict
         # key is PARNME value is PARGP
         pars_dict = {}
         for index, row in self.pardata.iterrows():
-            pars_dict[index] = row['PARGP']
+            pars_dict[index.lower()] = row['PARGP'].lower()
         
 
         # Build weights array
@@ -159,107 +163,10 @@ class ParSen(Pest):
         
         # Build pandas data frame of parameter sensitivities    
         sen_data = {'Sensitivity' : sensitivities, 'Parameter Group' : par_groups}
-        par_sen_df = pd.DataFrame(sen_data, index = jco_df.columns)
-        self.par_sen_df = par_sen_df
+        df = pd.DataFrame(sen_data, index = jco_df.columns)
+        self.df = df
 
 
-    def plot(self, n = None, group = None):
-        ''' Generate plot of parameter sensitivity
-        
-        Paramters
-        ----------
-        n: {None, int}, optional
-            If None then plot all parameters, else n is the number to plot.
-            If n is less than 0 then plot least sensitive parameters
-            If n is greater than 0 then plot most sensitive parameters
-        group: {None, str}, optional
-            Parameter group to plot           
-            If None plot all parameter groups
-                      
-        Returns
-        -------
-        Matplotlib plot
-            Bar plot of parameter sensitivity
-        '''
-        plt.figure() ### Make New figure
-        if group == None:    
-            if n == None:
-                n_head = len(self.par_sen_df.index)
-            else:
-                n_head = n
-            if n_head > 0:                        
-                pars = self.par_sen_df.sort(columns = 'Sensitivity',
-                                            ascending = False).head(n=n_head)\
-                                            ['Sensitivity'].index
-                sensitivity = self.par_sen_df.sort(columns = 'Sensitivity',\
-                                                   ascending = False)\
-                                                   .head(n=n_head)\
-                                                   ['Sensitivity'].values
-                par_groups = self.par_sen_df.sort(columns = 'Sensitivity',\
-                                                  ascending = False).\
-                                                  head(n=n_head)\
-                                                  ['Parameter Group'].values
-            if n_head < 0:
-                n_head = abs(n_head)
-                pars = self.par_sen_df.sort(columns = 'Sensitivity', 
-                                            ascending = False)\
-                                            .tail(n=n_head)\
-                                            ['Sensitivity'].index
-                sensitivity = self.par_sen_df.sort(columns = 'Sensitivity', 
-                                                   ascending = False)\
-                                                   .tail(n=n_head)\
-                                                   ['Sensitivity'].values
-                par_groups = self.par_sen_df.sort(columns = 'Sensitivity', 
-                                                  ascending = False).\
-                                                  tail(n=n_head)\
-                                                  ['Parameter Group'].values            
-    
-            # Assign colors for each group
-            color_map = plt.get_cmap('Spectral')
-            color_dict = dict()
-            unique_par_groups = np.asarray(self.par_sen_df.drop_duplicates\
-            (cols = 'Parameter Group')['Parameter Group'])
-            for i in range(len(unique_par_groups)):
-                color = color_map(1.*i/len(unique_par_groups))
-                color_dict[unique_par_groups[i]] = color
-            colors = []
-            for par_group in par_groups:            
-                colors.append(color_dict[par_group])
-            
-            plt.barh(np.arange(len(pars)), sensitivity, color = colors, 
-                     align = 'center')
-            plt.yticks(np.arange(len(pars)), pars)
-            plt.ylim(-1, len(pars))
-            plt.xlabel('Parameter Sensitivity')
-            plt.ylabel('Parameter') 
-            plt.grid(True, axis = 'x')
-            plt.tight_layout()
-            
-        if group != None:
-            group = group.lower()
-            if n == None:
-                n_head = len(self.par_sen_df.index)
-            else:
-                n_head = n
-            
-            if n_head > 0:            
-                pars = self.par_sen_df.sort(columns = 'Sensitivity', ascending = False)\
-                .ix[self.par_sen_df['Parameter Group'] == group, 'Sensitivity'].head(n=n_head).index
-                sensitivity = self.par_sen_df.sort(columns = 'Sensitivity', ascending = False)\
-                .ix[self.par_sen_df['Parameter Group'] == group, 'Sensitivity'].head(n=n_head).values          
-            if n_head < 0:
-                n_head = abs(n_head)            
-                pars = self.par_sen_df.sort(columns = 'Sensitivity', ascending = False)\
-                .ix[self.par_sen_df['Parameter Group'] == group, 'Sensitivity'].tail(n=n_head).index
-                sensitivity = self.par_sen_df.sort(columns = 'Sensitivity', ascending = False)\
-                .ix[self.par_sen_df['Parameter Group'] == group, 'Sensitivity'].tail(n=n_head).values                      
-            
-            plt.barh(np.arange(len(pars)), sensitivity, align = 'center')
-            plt.yticks(np.arange(len(pars)), pars)
-            plt.ylim(-1, len(pars))
-            plt.xlabel('Parameter Sensitivity')
-            plt.ylabel('Parameter')        
-            plt.tight_layout() 
             
     def tail(self, n_tail):
         ''' Get the lest sensitive parameters
@@ -274,7 +181,7 @@ class ParSen(Pest):
             Series of n_tail least sensitive parameters
                 
         '''
-        return self.par_sen_df.sort(columns = 'Sensitivity', ascending = False)\
+        return self.df.sort(columns = 'Sensitivity', ascending = False)\
         .tail(n=n_tail)['Sensitivity']
         
     def head(self, n_head):
@@ -289,7 +196,7 @@ class ParSen(Pest):
         pandas Series
             Series of n_head most sensitive parameters
         '''
-        return self.par_sen_df.sort(columns = 'Sensitivity', ascending = False)\
+        return self.df.sort(columns = 'Sensitivity', ascending = False)\
         .head(n=n_head)['Sensitivity']
 
     def par(self, parameter):
@@ -305,7 +212,7 @@ class ParSen(Pest):
             sensitivity of parameter
         
         '''
-        return self.par_sen_df.xs(parameter)['Sensitivity']
+        return self.df.xs(parameter)['Sensitivity']
         
     def group(self, group, n = None):
         '''Return the sensitivites of a parameter group
@@ -327,19 +234,19 @@ class ParSen(Pest):
         '''
         group = group.lower()
         if n == None:
-            n_head = len(self.par_sen_df.index)
+            n_head = len(self.df.index)
         else:
             n_head = n
         
         if n_head > 0:            
-            sensitivity = self.par_sen_df.sort(columns = 'Sensitivity', 
+            sensitivity = self.df.sort(columns = 'Sensitivity', 
                                                ascending = False)\
-                                               .ix[self.par_sen_df['Parameter Group'] == group].head(n=n_head)
+                                               .ix[self.df['Parameter Group'] == group].head(n=n_head)
         if n_head < 0:
             n_head = abs(n_head)            
-            sensitivity = self.par_sen_df.sort(columns = 'Sensitivity', 
+            sensitivity = self.df.sort(columns = 'Sensitivity', 
                                                ascending = False)\
-                                               .ix[self.par_sen_df['Parameter Group'] == group].tail(n=n_head)
+                                               .ix[self.df['Parameter Group'] == group].tail(n=n_head)
             
         sensitivity.index.name = 'Parameter'
         return sensitivity
@@ -351,33 +258,47 @@ class ParSen(Pest):
         -------
         Pandas DataFrame
         '''
-        sen_grouped = self.par_sen_df.groupby(['Parameter Group'])\
+        sen_grouped = self.df.groupby(['Parameter Group'])\
         .aggregate(np.sum).sort(columns = 'Sensitivity', ascending = False)
         return sen_grouped
+
         
-    def plot_sum_group (self):
-        ''' Plot sum of all parameters sensitivity by group
+    def plot(self, n = None, group = None, color_dict = None, alt_labels = None, **kwds):
+        if n == None:
+            n_head = len(self.df.index)
+        else:
+            n_head = n
+
+        if group == None:    
+
+            if n_head > 0:                        
+                sensitivity = self.df.sort(columns = 'Sensitivity', ascending = False).head(n=n_head)
+            if n_head < 0:
+                n_head = abs(n_head)
+                sensitivity = self.df.sort(columns = 'Sensitivity', ascending = False).tail(n=n_head)
+
+        if group != None:
+            group = group.lower()           
+            if n_head > 0:            
+                
+                sensitivity = self.df.sort(columns = 'Sensitivity', ascending = False).ix[self.df['Parameter Group'] == group].head(n=n_head)         
+            if n_head < 0:
+                n_head = abs(n_head)            
+                sensitivity = self.df.sort(columns = 'Sensitivity', ascending = False).ix[self.df['Parameter Group'] == group].tail(n=n_head)
+
+        if 'ylabel' not in kwds:
+            kwds['ylabel'] = 'Parameter Sensitivity'
+        if 'xlabel' not in kwds:
+            kwds['xlabel'] = 'Parameter'                   
+
+        plot_obj = plots.BarPloth(sensitivity, values_col = 'Sensitivity',
+                                  group_col = 'Parameter Group', color_dict = color_dict, alt_labels = alt_labels, **kwds)
+        plot_obj.generate()
+        plot_obj.draw()
+
+        return plot_obj.fig, plot_obj.ax
         
-        Returns
-        -------
-        Matplotlib plot
-            Bar plot of sum of sensitivity by parameter group
-        '''
-        plt.figure() ## Make New Figure
-        sen_grouped = self.par_sen_df.groupby(['Parameter Group'])\
-        .aggregate(np.sum).sort(columns = 'Sensitivity', ascending = False)
-        
-        pars = sen_grouped.index
-        sensitivity = sen_grouped.values
-        plt.barh(np.arange(len(pars)), sensitivity, align = 'center')
-        plt.yticks(np.arange(len(pars)), pars)
-        plt.ylim(-1, len(pars))
-        plt.xlabel('Sum of Parameter Sensitivity')
-        plt.ylabel('Parameter Group')
-        plt.grid(True, axis = 'x')        
-        plt.tight_layout() 
-        
-    def plot_mean_group (self):
+    def plot_mean_group (self, alt_labels = None, **kwds):
         ''' Plot mean of all parameters sensitivity by group
         
         Returns
@@ -385,17 +306,60 @@ class ParSen(Pest):
         Matplotlib plot
             Bar plot of mean of sensitivity by parameter group
         '''
-        plt.figure() ## Make New Figure
-        sen_grouped = self.par_sen_df.groupby(['Parameter Group'])\
-        .aggregate(np.mean).sort(columns = 'Sensitivity', ascending = False)
+        sen_grouped = self.df.groupby(['Parameter Group']).aggregate(np.mean).sort(columns = 'Sensitivity', ascending = False)
         
-        pars = sen_grouped.index
-        sensitivity = sen_grouped.values
-        plt.barh(np.arange(len(pars)), sensitivity, align = 'center')
-        plt.yticks(np.arange(len(pars)), pars)
-        plt.ylim(-1, len(pars))
-        plt.xlabel('Mean of Parameter Sensitivity')
-        plt.ylabel('Parameter Group')
-        plt.grid(True, axis = 'x')        
-        plt.tight_layout()
+        if 'ylabel' not in kwds:
+            kwds['ylabel'] = 'Mean of Parameter Sensitivity'
+        if 'xlabel' not in kwds:
+            kwds['xlabel'] = 'Parameter Group'
+
+        plot_obj = plots.BarPloth(sen_grouped, values_col = 'Sensitivity', alt_labels = alt_labels, **kwds)
+        plot_obj.generate()
+        plot_obj.draw()
+
+        return plot_obj.fig, plot_obj.ax
         
+    def plot_sum_group (self, alt_labels = None, **kwds):
+        ''' Plot sum of all parameters sensitivity by group
+        
+        Returns
+        -------
+        Matplotlib plot
+            Bar plot of sum of sensitivity by parameter group
+        '''
+
+        sen_grouped = self.df.groupby(['Parameter Group'])\
+        .aggregate(np.sum).sort(columns = 'Sensitivity', ascending = False)
+        
+        if 'ylabel' not in kwds:
+            kwds['ylabel'] = 'Sum of Parameter Sensitivity'
+        if 'xlabel' not in kwds:
+            kwds['xlabel'] = 'Parameter Group'
+        
+        
+        plot_obj = plots.BarPloth(sen_grouped, values_col = 'Sensitivity', alt_labels = alt_labels, **kwds)
+        plot_obj.generate()
+        plot_obj.draw()
+
+        return plot_obj.fig, plot_obj.ax
+        
+
+
+        
+        
+if __name__ == '__main__':
+    parsen = ParSen(r'C:\Users\egc\pest_tools-1\cc\columbia')
+    #pst = Pest(r'C:\Users\egc\pest_tools-1\cc\columbia')
+    alt_labels = {'r_lc' : "Full Parameter Description 'r_lc'"}
+    color_dict = {'kz': (0.89411765336990356, 0.10196078568696976, 0.10980392247438431, 1.0),
+                  'kp': 'g',
+                  'sfr_cond' : 'b',
+                  'rech' : 'k'}
+    parsen.plot(n=20)
+#    parsen.plot(n=20, color_dict = color_dict, alt_labels = alt_labels)
+#    parsen.plot_mean_group(color = 'k')
+#    parsen.plot(n=-20)
+#    parsen.plot(n=20, group = 'kz')
+#    parsen.plot_mean_group()
+#    parsen.plot_sum_group()
+    #parsen.plot(n=20, cmap = 'Set3')
