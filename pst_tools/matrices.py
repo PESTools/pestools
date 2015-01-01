b40c8d12ab77d47a7d6b7065e0144212b3fe8cdb
echristi <evan.christianson2@gmail.com>
2015-01-01 11:02:24 -0600

    Make matrices class since it holds more than cor data

--- pst_tools/matrices.py
+++ pst_tools/matrices.py
@@ -4,14 +4,15 @@ from pst import Pest
 import numpy as np
 import pandas as pd
 import plots
+import os
 import warnings
 
-class Cor(Pest):
+class Matrices(Pest):
 
     def __init__(self, basename, res_file=None, jco_df=None):
         Pest.__init__(self, basename)
 
-        ''' Create ParSen class
+        ''' Create Matrices class
 
         Parameters
         ----------
@@ -30,35 +31,56 @@ class Cor(Pest):
             Path to a .res  or .rei file to use to define the weights used to 
             calculate the parameter sensitivity.  If not provided it will look
             for basename+'.res'.  Weights are not taken from PEST control file
-            (.pst) because regularization weights in PEST conrtrol file do
-            not reflect the current weights.
-
-
+            (.pst)
 
         Attributes
         ----------
+        cov : array
+            array of the parameter covariance matrix
+        cov_df : DataFrame
+            DataFrame of the parameter covariance matrix
+        cor : array
+            array of the correlation coefficient matrix
+        cor_df : DataFrame
+            DataFrame of the correlation coefficient matrix
+        eig_values : array
+            array of the eigenvalues
+        eig_vectors : array
+            array of the eigenvectors matrix
+        eig_vectors_df : DataFrame
+            DataFrame of the eigenvectors matrix
 
         Methods
         -------
 
         Notes
         ------
+        As noted in Section 5.3.6 of the PEST manual, there may be slight
+        differences between matrixes recorded to the final matrix file (.mtt) 
+        from PEST and those listed in the run record file at the end of a PEST 
+        run. The same issue applies here.  "Reference variance" used to record 
+        values in the PEST matrix (.mtt) file is conputed using the objective 
+        function computed from the previous iteration.  The covariance matrices
+        recorded in the run record uses the best objective function value.
+        Covariance calculated with pestools uses the final .res file and .jco,
+        or any alternative jco, .rei, or .res if provided.  
+        The .mtt and .rec files are not used by  pestools.
+        
 
 
         '''
         
         # Check if reularization is used and warn the user
         # Note: Might want to come up with some better warning
-        if 'regularisation' in open(self.pstfile).read():
-            warnings.warn('Regularization used, statistical matrices may not applicable')
+#        if 'regularisation' in open(self.pstfile).read():
+#            warnings.warn('Regularization used, statistical matrices may not applicable')
             
         if jco_df is None:
             jco_df = self._load_jco()
-        self._read_par_data()
-        self._read_obs_data()
       
         if res_file is None:
-            res_file = self.pstfile.rstrip('pst')+'res'
+            res_file = os.path.splitext(self.pstfile)[0]+'.res'
+            #res_file = self.pstfile.rstrip('pst')+'res'
         # Check if .res or .rei
         try:
             check = open(res_file, 'r')
@@ -82,29 +104,26 @@ class Cor(Pest):
         # Calc Covarience Matrix
         # See eq. 2.17 in PEST Manual
         # Note: Number of observations are number of non-zero weighted observations
-        cov = np.dot((phi/(np.count_nonzero(weights)-len(pars))),
-                     (np.linalg.inv(np.dot(np.dot(jco_df.values.T, q),jco_df.values))))
-        
+        self.cov = np.dot((phi/(np.count_nonzero(weights)-len(pars))),
+                     (np.linalg.inv(np.dot(np.dot(jco_df.values.T, q),jco_df.values))))        
         # Put into dataframe
-        cov_df = pd.DataFrame(cov, index = pars, columns = pars)
+        self.cov_df = pd.DataFrame(self.cov, index = pars, columns = pars)
         
         # Calc correlation matrix   
-        d = np.diag(cov)
-        cor = cov/np.sqrt(np.multiply.outer(d,d))
+        d = np.diag(self.cov)
+        self.cor = self.cov/np.sqrt(np.multiply.outer(d,d))
         # Put into dataframe
-        cor_df = pd.DataFrame(cor, index = pars, columns = pars)
+        self.cor_df = pd.DataFrame(self.cor, index = pars, columns = pars)
         
         # Calc eigenvalues, eigenvectors
-        eig_values, eig_vectors = np.linalg.eigh(cov)
+        self.eig_values, self.eig_vectors = np.linalg.eigh(self.cov, UPLO = 'U')
         #Put eig_vectors into dataframe
-        eig_vectors_df = pd.DataFrame(eig_vectors, index = pars)
+        self.eig_vectors_df = pd.DataFrame(self.eig_vectors, index = pars)
         
-        
-        self.df = cor_df
-        self.array = cor
-        self.eig_vectors = eig_vectors_df
-        self.eig_values = eig_values
-        self.cov_df = cov_df
+
+
 #        
 if __name__ == '__main__':
-    cor = Cor(r'C:\Users\egc\pest_tools-1\cc\Columbia.pst')
+    #mtt = Matrices(r'C:\Users\egc\pest_tools-1\cc\Columbia.pst')
+    #mtt = Matrices(r'H:\Pest_Tools_Archive\pest_tools\Examples\cor_testing')
+    mtt = Matrices (r'C:\PEST\pest13\ppestex\test.pst')
